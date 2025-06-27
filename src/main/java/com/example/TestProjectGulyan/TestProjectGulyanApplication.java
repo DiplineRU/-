62ae3566790618dc347721d6eac2045cc12d4380
DiplineRU <artgul52@gmail.com>
2025-06-27 16:17:33 +0300

    init commit

---
+++ src/main/java/com/example/TestProjectGulyan/TestProjectGulyanApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestProjectGulyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectGulyanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectGulyanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestProjectGulyan/TestProjectGulyanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestProjectGulyan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectGulyanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
